7fae00795cbc0a8cf792b5d21def0b2de7fd389d
Nitish Deshmukh <nitishdeshmukh24@gmail.com>
2025-09-30 15:54:45 +0530

    Update MongoDB connection config

--- backend/config/mangodb.js
+++ backend/config/mangodb.js
@@ -4,8 +4,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("DB Connected");
   });
-  await mongoose.connect(`${process.env.MANGODB_URI}/ecommerce`);
+  await mongoose.connect(`${process.env.MANGODB_URI}/${process.env.DB_NAME}`);
 };
 
 export default connectDB;
-
